fe11122004ec0a0f0f6c7eeb696a930138b4e4c4
José Augusto <jafernandesalves220303@gmail.com>
2022-03-15 18:22:32 -0300

    Final Version

--- main.py
+++ main.py
@@ -5,24 +5,43 @@ from os import *
 from time import sleep
 
 
-
 matriz = [["", "", ""], 
           ["", "", ""], 
           ["", "", ""]] 
+
+team = ['X', 'O'] 
+
+system('cls')
+print('''0 -> X 
+1 -> O''')
+choose = int(input('Choose your team: ')) 
+person = team.pop(choose) 
+pc = team[0] 
 
 round = 1
 
 while True: 
     system('cls') 
+    # =========show the menu=========
     menu(matriz, round) 
-    fim = end(matriz) 
-    if fim:
+    # =============================== 
+
+    # ===========check if it´s the end of the game==============
+    if any([check_lines(matriz, [person, pc]), check_columns(matriz, [person, pc]), check_diagonal_l(matriz, [person, pc]), check_diagonal_r(matriz, [person, pc])]) or end(matriz): 
+        print('-'*24)
         break
-    sit_player = player(matriz) 
-    if sit_player is True: 
-        sit_cpu = cpu(matriz) 
-        round += 1 
-    else: 
+    # =========================================================== 
+
+    # =========input of the game========  
+    try:
+        sit_player = player(matriz, [person, pc]) 
+        if sit_player is True: 
+            cpu(matriz, [person, pc]) 
+            round += 1 
+        else: 
+            continue 
+    except: 
         continue
+    # ================================== 
     sleep(1) 
 
--- menu.py
+++ menu.py
@@ -10,35 +10,121 @@ def menu(container: list, r: int) -> None:
             print(c + ' | ', end='') 
         print('\n-----------')
         cont += 1 
-    print(f"Round: {r}")
 
-def cpu(matriz: list) -> None:
+def cpu(matriz: list, team: list) -> bool:
     cont = 0
     playCpu_cell = randrange(0, len(matriz))
     playCpu_c = randrange(0, len(matriz[playCpu_cell])) 
     while True:
         if matriz[playCpu_cell][playCpu_c] == '': 
-            matriz[playCpu_cell][playCpu_c] = 'X' 
+            matriz[playCpu_cell][playCpu_c] = team[1] 
             break
         else: 
             playCpu_cell = randrange(0, len(matriz))
             playCpu_c = randrange(0, len(matriz[playCpu_cell])) 
             cont += 1 
         if cont > 10: 
-            break
-    return True
+            break 
     
 
-def player(matriz: list) -> None: 
+def player(matriz: list, team: list) -> bool: 
     line = int(input('Line: ')) 
     column = int(input('Column: ')) 
     while True:
         if matriz[line][column] == '':
-            matriz[line][column] = 'O' 
+            matriz[line][column] = team[0] 
             break
         else: 
             return False 
-    return True
+    return True 
+
+def check_lines(matriz: list, team: list) -> bool: 
+    line_o = [] 
+    line_x = [] 
+    for cell in matriz: 
+        line_o = [o for o in cell if o == 'O'] 
+        line_x = [x for x in cell if x == 'X'] 
+        if len(line_o) == 3 and len(line_x) == 0: 
+            if team[0] == line_o[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_o[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True 
+        if len(line_x) == 3 and len(line_o) == 0: 
+            if team[0] == line_x[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_x[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True
+    return False 
+
+def check_columns(matriz: list, team: list) -> bool:
+    for c in range(3): 
+        line_o = [] 
+        line_x = []
+        for cell in range(3): 
+            if matriz[cell][c] == 'O': 
+                line_o.append(matriz[cell][c]) 
+            if matriz[cell][c] == 'X':
+                line_x.append(matriz[cell][c]) 
+        if len(line_o) == 3 and len(line_x) == 0: 
+            if team[0] == line_o[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_o[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True 
+        if len(line_x) == 3 and len(line_o) == 0: 
+            if team[0] == line_x[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_x[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True 
+
+def check_diagonal_r(matriz: list, team: list) -> bool: 
+    line_o = [] 
+    line_x = [] 
+    for i in range(3): 
+        if matriz[i][i] == 'O': 
+            line_o.append(matriz[i][i]) 
+        if matriz[i][i] == 'X':
+            line_x.append(matriz[i][i]) 
+    if len(line_o) == 3 and len(line_x) == 0: 
+            if team[0] == line_o[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_o[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True 
+    if len(line_x) == 3 and len(line_o) == 0: 
+            if team[0] == line_x[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_x[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True 
+    return False 
+
+def check_diagonal_l(matriz: list, team: list) -> bool: 
+    line_o = [] 
+    line_x = [] 
+    cont = 0
+    for i in range(2, -1, -1): 
+        if matriz[i][cont] == 'O': 
+            line_o.append(matriz[i][cont]) 
+        if matriz[i][cont] == 'X':
+            line_x.append(matriz[i][cont]) 
+        cont += 1
+    if len(line_o) == 3 and len(line_x) == 0: 
+            if team[0] == line_o[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_o[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True 
+    if len(line_x) == 3 and len(line_o) == 0: 
+            if team[0] == line_x[0]: 
+                print('THE PLAYER HAS GAINED!')
+            if team[1] == line_x[0]: 
+                print('THE COMPUTER HAS GAINED!')
+            return True 
+    return False
 
 def end(matriz: list) -> bool: 
     cont = 0
